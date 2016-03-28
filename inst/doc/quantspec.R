f1efa2dbae400647e4000ace29e0dcf4417f5db0
Tobias Kley <t.kley@lse.ac.uk>
2016-03-28 00:27:55 +0000

    version 1.2-1

--- inst/doc/quantspec.R
+++ inst/doc/quantspec.R
@@ -8,45 +8,45 @@ set.seed(2581)
 
 
 ###################################################
-### code chunk number 2: quantspec.Rnw:581-582
+### code chunk number 2: quantspec.Rnw:735-736
 ###################################################
 library("quantspec")
 
 
 ###################################################
-### code chunk number 3: quantspec.Rnw:588-589 (eval = FALSE)
+### code chunk number 3: quantspec.Rnw:742-743 (eval = FALSE)
 ###################################################
 ## help("quantspec")
 
 
 ###################################################
-### code chunk number 4: quantspec.Rnw:604-606
+### code chunk number 4: quantspec.Rnw:757-759
 ###################################################
 Y  <- rnorm(8)
 bn <- qRegEstimator(Y, levels = c(0.25, 0.5, 0.75))
 
 
 ###################################################
-### code chunk number 5: quantspec.Rnw:613-614
+### code chunk number 5: quantspec.Rnw:766-767
 ###################################################
 bn
 
 
 ###################################################
-### code chunk number 6: quantspec.Rnw:627-629
+### code chunk number 6: quantspec.Rnw:779-781
 ###################################################
 getFrequencies(bn)
 getParallel(bn)
 
 
 ###################################################
-### code chunk number 7: quantspec.Rnw:638-639
+### code chunk number 7: quantspec.Rnw:790-791
 ###################################################
 getValues(bn, levels = c(0.25, 0.5))
 
 
 ###################################################
-### code chunk number 8: quantspec.Rnw:647-648 (eval = FALSE)
+### code chunk number 8: quantspec.Rnw:799-800 (eval = FALSE)
 ###################################################
 ## help("getValues-FreqRep")
 
@@ -59,14 +59,14 @@ getValues(bn, levels = c(0.25, 0.5))
 
 
 ###################################################
-### code chunk number 10: quantspec.Rnw:668-669
+### code chunk number 10: quantspec.Rnw:820-821
 ###################################################
 dn <- clippedFT(rnorm(32), levels = seq(0.05, 0.95, 0.05))
 plot(dn, frequencies = 2 * pi * (0:64) / 32, levels = c(0.25, 0.5))
 
 
 ###################################################
-### code chunk number 11: quantspec.Rnw:705-708 (eval = FALSE)
+### code chunk number 11: quantspec.Rnw:857-860 (eval = FALSE)
 ###################################################
 ## demo("sp500")
 ## demo("wheatprices")
@@ -83,7 +83,7 @@ plot(dn, frequencies = 2 * pi * (0:64) / 32, levels = c(0.25, 0.5))
 
 
 ###################################################
-### code chunk number 13: quantspec.Rnw:755-760
+### code chunk number 13: quantspec.Rnw:909-914
 ###################################################
 def.par <- par(no.readonly = TRUE) # save default, for resetting...
 layout(matrix(c(1,1,2,3), nrow=1))
@@ -133,13 +133,13 @@ par(def.par)  #- reset to default
 
 
 ###################################################
-### code chunk number 18: quantspec.Rnw:867-868 (eval = FALSE)
+### code chunk number 18: quantspec.Rnw:1025-1026 (eval = FALSE)
 ###################################################
 ## help("plot-SmoothedPG")
 
 
 ###################################################
-### code chunk number 19: quantspec.Rnw:922-923
+### code chunk number 19: quantspec.Rnw:1083-1084
 ###################################################
 CR <- quantilePG(sp500, levels.1 = c(0.05, 0.5, 0.95),
   type = "clipped", type.boot = "mbb", B = 250, l = 32)
@@ -150,7 +150,7 @@ plot(CR, levels = c(0.05, 0.5, 0.95),
 
 
 ###################################################
-### code chunk number 20: quantspec.Rnw:932-933
+### code chunk number 20: quantspec.Rnw:1093-1094
 ###################################################
 plot(CR, levels = c(0.05, 0.5, 0.95),
   frequencies = freq[freq > 0 & freq <= pi/5],
@@ -158,7 +158,7 @@ plot(CR, levels = c(0.05, 0.5, 0.95),
 
 
 ###################################################
-### code chunk number 21: quantspec.Rnw:946-947
+### code chunk number 21: quantspec.Rnw:1107-1108
 ###################################################
 sPG <- smoothedPG(CR, weight = kernelWeight(W = W1, bw = 0.07))
 plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "individual",
@@ -167,7 +167,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "individual",
 
 
 ###################################################
-### code chunk number 22: quantspec.Rnw:957-958
+### code chunk number 22: quantspec.Rnw:1118-1119
 ###################################################
 plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
   ptw.CIs = 0.1, type.CIs = "boot.full",
@@ -176,7 +176,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
 
 
 ###################################################
-### code chunk number 23: quantspec.Rnw:997-998 (eval = FALSE)
+### code chunk number 23: quantspec.Rnw:1158-1159 (eval = FALSE)
 ###################################################
 ## help("ts-models")
 
@@ -198,7 +198,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
 
 
 ###################################################
-### code chunk number 26: quantspec.Rnw:1036-1037 (eval = FALSE)
+### code chunk number 26: quantspec.Rnw:1199-1200 (eval = FALSE)
 ###################################################
 ## help("increasePrecision-QuantileSD")
 
@@ -212,7 +212,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
 
 
 ###################################################
-### code chunk number 28: quantspec.Rnw:1058-1063 (eval = FALSE)
+### code chunk number 28: quantspec.Rnw:1221-1226 (eval = FALSE)
 ###################################################
 ## sCR <- smoothedPG(ts1(512), levels.1 = c(0.25, 0.5, 0.75),
 ##   weight = kernelWeight(W = W1, bw = 0.1))
@@ -222,7 +222,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
 
 
 ###################################################
-### code chunk number 29: quantspec.Rnw:1078-1092 (eval = FALSE)
+### code chunk number 29: quantspec.Rnw:1241-1255 (eval = FALSE)
 ###################################################
 ## set.seed(2581)
 ## ts <- ts1
@@ -241,7 +241,7 @@ plot(sPG, levels = c(0.05, 0.5, 0.95), type.scaling = "real-imaginary",
 
 
 ###################################################
-### code chunk number 30: quantspec.Rnw:1111-1112
+### code chunk number 30: quantspec.Rnw:1274-1275
 ###################################################
 csd <- quantileSD(N = 2^9, seed.init = 2581, type = "copula",
   ts = ts1, levels.1 = c(0.25, 0.5, 0.75), R = 100, quiet = TRUE)
@@ -249,7 +249,7 @@ plot(csd, ylab = expression(f[list(q[tau[1]], q[tau[2]])](omega)))
 
 
 ###################################################
-### code chunk number 31: quantspec.Rnw:1137-1143
+### code chunk number 31: quantspec.Rnw:1300-1306
 ###################################################
 set.seed(020581)
 sCR <- smoothedPG(ts1(512), levels.1 = c(0.25, 0.5, 0.75),
@@ -260,16 +260,14 @@ plot(sCR, qsd = csd,
 
 
 ###################################################
-### code chunk number 32: quantspec.Rnw:1156-1169 (eval = FALSE)
+### code chunk number 32: quantspec.Rnw:1319-1330 (eval = FALSE)
 ###################################################
 ## for (i in 1:R) {
 ##   Y <- ts(N)
-## 
 ##   CR  <- quantilePG(Y, levels.1 = levels, type = "clipped")
 ##   LP  <- quantilePG(Y, levels.1 = levels, type = "qr")
 ##   sCR <- smoothedPG(CR, weight = weight)
 ##   sLP <- smoothedPG(LP, weight = weight)
-## 
 ##   sims[1, i, , , ] <- getValues(CR,  frequencies = freq)[, , , 1]
 ##   sims[2, i, , , ] <- getValues(LP,  frequencies = freq)[, , , 1]
 ##   sims[3, i, , , ] <- getValues(sCR, frequencies = freq)[, , , 1]
@@ -278,7 +276,7 @@ plot(sCR, qsd = csd,
 
 
 ###################################################
-### code chunk number 33: quantspec.Rnw:1183-1187 (eval = FALSE)
+### code chunk number 33: quantspec.Rnw:1344-1348 (eval = FALSE)
 ###################################################
 ## trueV <- getValues(csd, frequencies = freq)
 ## SqDev <- array(apply(sims, c(1, 2),
